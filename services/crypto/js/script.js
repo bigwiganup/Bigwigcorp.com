eeba8b404241739bd2b58a5e5111ff0eb0eb90d6
BigWigHarshad <Harshad.gaikwad@bigwigcorp.com>
2024-06-10 14:10:44 +0530

    create subpages

--- services/crypto/js/script.js
+++ services/crypto/js/script.js
@@ -1,3 +1,9 @@
+var loader = document.getElementById("preloader");
+
+  window.addEventListener("load", function(){
+    loader.style.display="none";
+  });
+
 
 $(document).ready(function(){
     $("#Header_button").click(function(){
